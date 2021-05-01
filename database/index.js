769d88510c2b4513dbb7f10af36e4d30b5aea1c4
tarrinneal <tarrinneal@gmail.com>
2021-05-01 16:54:51 -0700

    starting over

--- database/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect(process.env.DBTOKEN);
-
-const stuffSchema = mongoose.Schema({});
-
-let Stuff = mongoose.model('stuff', stuffSchema);
-
-module.exports = {};
