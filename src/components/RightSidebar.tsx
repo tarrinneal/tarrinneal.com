ba1d8c136e880604b4924588e2b827c3c29bb7b7
tarrinneal <tarrinneal@gmail.com>
2021-05-05 11:44:54 -0700

    added portfolio sidebar

---
+++ src/components/RightSidebar.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+export const RightSidebar = () => (
+  <div id='rightSidebar'>
+    <h2 id='portfolioHeader'>Portfolio</h2>
+    <h3 className='portfolioName'>Pacific Microphone</h3>
+    <p className='portfolioDescription'>App For Actors</p>
+    <h3 className='portfolioName'>Hopr</h3>
+    <p className='portfolioDescription'>Ecommerce Front End</p>
+    <h3 className='portfolioName'>Lily Pad</h3>
+    <p className='portfolioDescription'>Ecommerce Back End Redesign</p>
+    <h3 className='portfolioName'>Triple Triad</h3>
+    <p className='portfolioDescription'>Remade Final Fantasy VIII Card Game</p>
+    <h3 className='portfolioName'>Tarrinneal.com</h3>
+    <p className='portfolioDescription'>This Website!</p>
+  </div>
+);
--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,4 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { App } from './components/App';
 
-ReactDOM.render(<App num={1337} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
