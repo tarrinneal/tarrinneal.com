4c33f92072a820b989549c20a6c8a9442a06d5e0
tarrinneal <tarrinneal@gmail.com>
2021-05-05 17:12:20 -0700

    added linkedIn link in the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,17 @@
 import * as React from 'react';
+import { GrLinkedin } from 'react-icons/gr';
 
 export const Header = () => (
   <div id='header'>
     <h1 id='headerText'>This is Tarrin Neal's website! Please hire him!</h1>
+    <div id='icons'>
+      <a
+        href='https://www.linkedin.com/in/tarrinneal/'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <GrLinkedin id='linkedInIcon' className='icon' />
+      </a>
+    </div>
   </div>
 );
