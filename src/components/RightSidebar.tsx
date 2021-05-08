6d3f44b96d096ed8e1ead650960f6182a88698c3
tarrinneal <tarrinneal@gmail.com>
2021-05-07 20:53:38 -0700

    changed pointer on hover of 'buttons' and removed a tag from text on right sidebar

--- src/components/RightSidebar.tsx
+++ src/components/RightSidebar.tsx
@@ -5,20 +5,18 @@ export const RightSidebar = () => (
     <h2 id='portfolioHeader'>Portfolio</h2>
     <a href='https://pacificmicroph.one'>
       <h3 className='portfolioName'>Pacific Microphone</h3>
-      <p className='portfolioDescription'>App For Actors</p>
     </a>
+    <p className='portfolioDescription'>App For Actors</p>
     <a href='https://fec-hopper.herokuapp.com'>
       <h3 className='portfolioName'>Hopr</h3>
-      <p className='portfolioDescription'>Ecommerce Front End</p>
     </a>
+    <p className='portfolioDescription'>Ecommerce Front End</p>
     <h3 className='portfolioName'>Lily Pad</h3>
     <p className='portfolioDescription'>Ecommerce Back End Redesign</p>
     <a href='https://triple-triad-mvp.herokuapp.com/'>
       <h3 className='portfolioName'>Triple Triad</h3>
-      <p className='portfolioDescription'>
-        Remade Final Fantasy VIII Card Game
-      </p>
     </a>
+    <p className='portfolioDescription'>Remade Final Fantasy VIII Card Game</p>
     <h3 className='portfolioName'>Tarrinneal.com</h3>
     <p className='portfolioDescription'>This Website!</p>
   </div>
