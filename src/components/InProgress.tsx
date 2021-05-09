56e6858f2c9e940df5f5293e073d368996d79a66
tarrinneal <tarrinneal@gmail.com>
2021-05-09 15:34:41 -0700

    added more of my old and or unfinished projects to the in progress tab

--- src/components/InProgress.tsx
+++ src/components/InProgress.tsx
@@ -12,9 +12,42 @@ export const InProgress = () => (
         Cracking the Coding Interview Practice Repo
       </h3>
     </a>
-    <p className='blogDescription'>
+    <p className='inProgressDescription'>
       A practice repo with all practice problems from the book Cracking the
       Coding Interview, with hints, solutions, and tests for every problem
+    </p>
+    <a
+      href='https://bad-libs.herokuapp.com/'
+      target='_blank'
+      rel='noopener noreferrer'
+    >
+      <h3 className='inProgressTitle'>Ad Lib Builder</h3>
+    </a>
+    <p className='inProgressDescription'>
+      A wep app that allows users to create and upload new Ad Lib's as well as
+      create stories using others uploaded Ad Libs's!
+    </p>
+    <a
+      href='https://lil-buds.herokuapp.com/'
+      target='_blank'
+      rel='noopener noreferrer'
+    >
+      <h3 className='inProgressTitle'>Lil Buds</h3>
+    </a>
+    <p className='inProgressDescription'>
+      A pokemone style battler with some of my classmates pets! Custom recorded
+      sfx, animations and stats for every move and 'bud'. Built with Jquery and
+      Css.
+    </p>
+    <a
+      href='https://bar-gle-cade.herokuapp.com/'
+      target='_blank'
+      rel='noopener noreferrer'
+    >
+      <h3 className='inProgressTitle'>Arcade</h3>
+    </a>
+    <p className='inProgressDescription'>
+      Some old games I made when I was first learning JS basics.
     </p>
   </div>
 );
