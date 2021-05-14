27a419c5c8cd2e2142aef41af068b1cf3c0248aa
tarrinneal <tarrinneal@gmail.com>
2021-05-14 11:08:54 -0700

    minor server changes to fix router

--- server/index.js
+++ server/index.js
@@ -4,10 +4,46 @@ const port = process.env.PORT || 3005;
 require('dotenv').config();
 const db = require('../database');
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('');
+  res.sendFile('index.html', { root: __dirname + '/../public/' }, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('sent');
+    }
+  });
+});
+
+app.get('/styles.css', (req, res) => {
+  res.sendFile('styles.css', { root: __dirname + '/../public/' }, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('sent');
+    }
+  });
+});
+
+app.get('/app/bundle.js', (req, res) => {
+  res.sendFile('bundle.js', { root: __dirname + '/../public/app/' }, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('sent');
+    }
+  });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile('index.html', { root: __dirname + '/../public/' }, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('sent');
+    }
+  });
 });
 
 app.listen(port, () => {
