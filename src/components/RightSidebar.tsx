b3a9bbf13a891e8953890dd88f32ba390567d532
tarrinneal <tarrinneal@gmail.com>
2021-05-17 13:25:36 -0700

    added recursion joke

--- src/components/RightSidebar.tsx
+++ src/components/RightSidebar.tsx
@@ -1,35 +1,49 @@
 import * as React from 'react';
+import { App } from './App';
 
-export const RightSidebar = () => (
-  <div id='rightSidebar'>
-    <h2 id='portfolioHeader'>Portfolio</h2>
-    <a
-      href='https://pacificmicroph.one'
-      target='_blank'
-      rel='noopener noreferrer'
-    >
-      <h3 className='portfolioName'>Pacific Microphone</h3>
-    </a>
-    <p className='portfolioDescription'>App For Actors</p>
-    <a
-      href='https://fec-hopper.herokuapp.com'
-      target='_blank'
-      rel='noopener noreferrer'
-    >
-      <h3 className='portfolioName'>Hopr</h3>
-    </a>
-    <p className='portfolioDescription'>Ecommerce Front End</p>
-    <h3 className='portfolioName'>Lily Pad</h3>
-    <p className='portfolioDescription'>Ecommerce Back End Redesign</p>
-    <a
-      href='https://triple-triad-mvp.herokuapp.com/'
-      target='_blank'
-      rel='noopener noreferrer'
-    >
-      <h3 className='portfolioName'>Triple Triad</h3>
-    </a>
-    <p className='portfolioDescription'>Remade Final Fantasy VIII Card Game</p>
-    <h3 className='portfolioName'>Tarrinneal.com</h3>
-    <p className='portfolioDescription'>This Website!</p>
-  </div>
-);
+export const RightSidebar = () => {
+  const [recurse, setRecurse] = React.useState(false);
+  return (
+    <div id='rightSidebar'>
+      <h2 id='portfolioHeader'>Portfolio</h2>
+      <a
+        href='https://pacificmicroph.one'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <h3 className='portfolioName'>Pacific Microphone</h3>
+      </a>
+      <p className='portfolioDescription'>App For Actors</p>
+      <a
+        href='https://fec-hopper.herokuapp.com'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <h3 className='portfolioName'>Hopr</h3>
+      </a>
+      <p className='portfolioDescription'>Ecommerce Front End</p>
+      <h3 className='portfolioName'>Lily Pad</h3>
+      <p className='portfolioDescription'>Ecommerce Back End Redesign</p>
+      <a
+        href='https://triple-triad-mvp.herokuapp.com/'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <h3 className='portfolioName'>Triple Triad</h3>
+      </a>
+      <p className='portfolioDescription'>
+        Remade Final Fantasy VIII Card Game
+      </p>
+      <h3
+        className='portfolioName recurse'
+        onClick={() => {
+          setRecurse(!recurse);
+        }}
+      >
+        Tarrinneal.com
+      </h3>
+      <p className='portfolioDescription'>This Website!</p>
+      {recurse ? <App /> : null}
+    </div>
+  );
+};
