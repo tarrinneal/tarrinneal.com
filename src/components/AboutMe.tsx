653bbc808d35a790d625367a96a6935db45b37db
tarrinneal <tarrinneal@gmail.com>
2021-05-19 23:56:02 -0700

    about me

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -4,7 +4,17 @@ export const AboutMe = () => (
   <div id='aboutMe'>
     <h2 className='aboutMeHeader'>Who am I?</h2>
     <p className='aboutMeText'>
-      My name is Tarrin, and I am a fullstack software engineer.
+      My name is Tarrin, and I am a fullstack software engineer. I learned on my
+      own off and on for a few years, then decided it was time to really take a
+      leap of faith and joined Hack Reactor's full time immersive program.
+      <br />
+      <br />
+      Before joining Hack Reactor I was always worried that I wasn't good enough
+      or smart enough to be a software engineer, but in the program I realized
+      this is what I was going to do for the rest of my life. I excelled much
+      more than I could have hoped for, to the point were I was spending large
+      amounts of my time helping the other students with their assignments
+      throughout the program.
     </p>
   </div>
 );
